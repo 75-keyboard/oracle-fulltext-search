100468a0a3708b27a043b8d662111992da760eea
75-akimas <akms2679@gmail.com>
2019-10-28 09:35:20 +0900

    Create factor oracle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{ HashMap, HashSet };
+use std::collections::{ HashMap, HashSet, VecDeque };
 
 fn main(){
     let mut s = Vec::new();
@@ -9,6 +9,7 @@ fn main(){
     let mut h = HashMap::new();
     let mut hs = HashSet::new();
     let mut last = 0;
+    let mut dist: Vec<Vec<(usize, usize)>> = Vec::new();
     for i in 0..s.len() {
         let mut crt = 0;
         for j in 0..s[i].len() {
@@ -16,8 +17,11 @@ fn main(){
             match h.get(&(crt, s[i][j])) {
                 Some(x) => {
                     crt=*x;
+                    dist[crt-1].push((i, j));
                 },
                 None => {
+                    dist.push(Vec::new());
+                    dist[last].push((i, j));
                     last+=1;
                     h.insert((crt, s[i][j]), last);
                     crt=last;
@@ -26,9 +30,41 @@ fn main(){
         }
     }
 
-
     println!("{:?}", h);
     println!("{:?}", hs);
+    println!("{:?}", dist);
 
-    
+    let mut fa_states: Vec<Vec<usize>> = Vec::new();
+    fa_states.push((0..last).collect::<Vec<usize>>());
+    let mut fa_trans = HashMap::new();
+
+    let mut i = 0;
+    while fa_states.len() > i {
+        'outer: for t in hs.iter() {
+            let mut tmp = Vec::new();
+            for s in fa_states[i].iter() {
+                match h.get(&(*s, **t)) {
+                    Some(x) => {
+                        tmp.push(*x);
+                    },
+                    None => {}
+                }
+            }
+            if tmp.len() > 0 {
+                tmp.sort();
+                'inner: for u in fa_states.iter() {
+                    if tmp[0] == u[0] {
+                        fa_trans.insert((fa_states[i].clone(), t), u.clone());
+                        continue 'outer;
+                }
+            }
+                fa_trans.insert((fa_states[i].clone(), t), tmp.clone());
+                fa_states.push(tmp.clone());
+            }
+        }
+        i+=1;
+    }
+    println!("{:?}", fa_states);
+    println!("--------------------------------");
+    println!("{:?}", fa_trans);
 }
